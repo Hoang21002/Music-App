655125373e34df584a3b816c182e07df56779502
Hoang21002 <dinhhoang026@gmail.com>
2023-10-06 01:18:30 +0700

    feat: conect firebase

--- music-app/src/environments/environment.prod.ts
+++ music-app/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'musicappdatabase',
+    appId: '1:1030106513519:web:0914da2879019d0f65cd9a',
+    storageBucket: 'musicappdatabase.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDd25MDi11Vdu68ZgIYrlW6JCHJwrr13yA',
+    authDomain: 'musicappdatabase.firebaseapp.com',
+    messagingSenderId: '1030106513519',
+    measurementId: 'G-XRN403MY8G',
+  },
   production: true
 };
--- music-app/src/environments/environment.ts
+++ music-app/src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'musicappdatabase',
+    appId: '1:1030106513519:web:0914da2879019d0f65cd9a',
+    storageBucket: 'musicappdatabase.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDd25MDi11Vdu68ZgIYrlW6JCHJwrr13yA',
+    authDomain: 'musicappdatabase.firebaseapp.com',
+    messagingSenderId: '1030106513519',
+    measurementId: 'G-XRN403MY8G',
+  },
   production: false
 };
 
