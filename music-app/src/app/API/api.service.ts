96f753887616d0c2994362d5a7abbe79df2f2daa
Hoang21002 <dinhhoang026@gmail.com>
2023-10-07 04:12:23 +0700

    feat: create function get data

--- music-app/src/app/API/api.service.ts
+++ music-app/src/app/API/api.service.ts
@@ -8,24 +8,93 @@ import { environment } from 'src/environments/environment';
 })
 export class ApiService implements OnInit {
   dataAll: any[] = [];
+  dataName : any[] = []
   constructor() { }
   async ngOnInit() {
     await this.GetApi()
   }
-  async GetApi(): Promise<void> {
+  async GetApi(): Promise<any> {
     try {
 
       const firebaseApp = initializeApp(environment.firebase);
       const firestore = getFirestore(firebaseApp);
       const collectionReference = collection(firestore, 'artists');
       const querySnapshot = await getDocs(collectionReference);
+      const database = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));   
 
-      this.dataAll = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-    
+      return database
+
     } catch (error) {
-
       console.error('Error getting documents: ', error);
     }
+  }
+
+  async GetDataAll(){
+    const dataAll = await this.GetApi()
+    return dataAll
+  }
+  // Get Name Singer
+  async GetDataNameSinger(){
+    const dataAll = await this.GetApi()
+    const dataName =  this.GetValues('name',dataAll)
+    return dataName
+  }
+  // Get Genre Albums
+  async GetDataGenre(){
+    const dataAll = await this.GetApi()
+    const dataGenre =  this.GetValues('genre',dataAll)
+    return dataGenre
+  }
+  // Get Img Singer
+  async GetDataImgSinger(){
+    const dataAll = await this.GetApi()
+    const dataImgsinger =  this.GetValues('imgsinger',dataAll)
+    return dataImgsinger
+  }
+  // Get Albums
+  async GetDataAlbums(){
+    const dataAll = await this.GetApi()
+    const dataAlbums =  this.GetValues('albums',dataAll)
+    return dataAlbums
+  }
+  async GetDataImgAlbums(){
+    const dataAlbums = await this.GetDataAlbums()
+    const getValues = this.GetValuesAlbums('cover',dataAlbums)
+    return getValues
+  }
+  // Get Name Albums
+  async GetDataNamesAlbums(){
+    const dataAlbums = await this.GetDataAlbums()
+    const getValues = this.GetValuesAlbums('title',dataAlbums)
+    return getValues
 
   }
+  // Get Name Songs in Albums
+  async GetDataSongs(){
+    const dataAlbums = await this.GetDataAlbums()
+    const getValues = this.GetValuesAlbums('songs',dataAlbums)
+    const NameSongs = this.GetValuesAlbums('title',getValues)
+
+    return NameSongs
+    
+  }
+  // Funtion loop use get value in albums
+  GetValuesAlbums(key: string,array: any){
+    const getValues = []
+    for (let index = 0; index < array.length; index++) {
+      for (let a = 0; a < array[index].length; a++) {
+        getValues.push(array[index][a][key]);
+      }      
+    }
+    return getValues
+  }
+  // Funtion loop use get value in artists
+  GetValues(key : string, array : any){
+    const getValues = []
+    for (let index = 0; index < array.length; index++) {
+      getValues.push(array[index][key]);
+    }
+    return getValues
+  }
+
 }
--- music-app/src/app/start/start.page.ts
+++ music-app/src/app/start/start.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../API/api.service';
 
 @Component({
   selector: 'app-start',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartPage implements OnInit {
 
-  constructor() { }
+  imgAlbums : any[]=[]
+  nameAlbums : any[]=[]
+  cards: any[] = [];
+  constructor(private apiservice: ApiService) { }
+
 
   ngOnInit() {
+    Promise.all([this.GetDataImgAlbums(), this.GetDataNamesAlbums()])
+    .then(() => this.CreateCard());
   }
+  async GetDataImgAlbums(): Promise<any>{
+    const data = await this.apiservice.GetDataImgAlbums()
+    for (let index = 0; index < 8; index++) {
+      this.imgAlbums.push(data[index]);
+    }
+    console.log(this.imgAlbums)
+    // console.log(data)
+  }
+  async GetDataNamesAlbums(){
+    const data = await this.apiservice.GetDataNamesAlbums()
+    for (let index = 0; index < 8; index++) {
+      this.nameAlbums.push(data[index]);
+    }
+    console.log(this.nameAlbums)
 
+  }
+  async CreateCard(){
+    this.cards = this.imgAlbums.map((img, index) => ({
+      img: img,
+      title: this.nameAlbums[index]
+    }));
+  }
 }
