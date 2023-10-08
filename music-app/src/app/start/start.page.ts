e9ccaa9107d460124a6888f0ae4f87f20523fb87
Hoang21002 <dinhhoang026@gmail.com>
2023-10-08 12:01:15 +0700

    feat: fix slides

--- music-app/src/app/start/start.page.ts
+++ music-app/src/app/start/start.page.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, OnInit,ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ApiService } from '../API/api.service';
 import { Swiper } from 'swiper/types';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 @Component({
   selector: 'app-start',
   templateUrl: './start.page.html',
@@ -8,26 +9,36 @@ import { Swiper } from 'swiper/types';
 
 })
 export class StartPage implements OnInit {
-
   imgAlbums: any[] = []
   nameAlbums: any[] = []
   cards: any[] = [];
+  genres:any[]=[]
+  // isSelect: boolean = false
+  isSelected: boolean[] = Array(this.cards.length).fill(false);
   constructor(private apiservice: ApiService) { }
 
 
-  ngOnInit() {
+  async ngOnInit() {
 
-    Promise.all([this.GetDataImgAlbums(), this.GetDataNamesAlbums()])
+    Promise.all([this.GetDataImgAlbums(), this.GetDataNamesAlbums(),this.GetDataGenre()])
       .then(() => this.CreateCard());
-    this.GetDataNamesAlbums()
-
   }
-  changeslide(){
-    
+
+  AddClass(index:number){
+    this.isSelected[index] = !this.isSelected[index];
+  }
+  Nextslide() {
+    const swiperEl = document.querySelector('swiper-container');
+    swiperEl?.swiper.slideNext()
+  }
+
+  Prevslide() {
+    const swiperEl = document.querySelector('swiper-container');
+    swiperEl?.swiper.slidePrev()
   }
   async GetDataImgAlbums(): Promise<any> {
     const data = await this.apiservice.GetDataImgAlbums()
-    for (let index = 0; index < 8; index++) {
+    for (let index = 0; index < data.length; index++) {
       this.imgAlbums.push(data[index]);
     }
     console.log(this.imgAlbums)
@@ -35,15 +46,22 @@ export class StartPage implements OnInit {
   }
   async GetDataNamesAlbums() {
     const data = await this.apiservice.GetDataNamesAlbums()
-    for (let index = 0; index < 8; index++) {
+    for (let index = 0; index < data.length; index++) {
       this.nameAlbums.push(data[index]);
     }
     console.log(this.nameAlbums)
   }
+  async GetDataGenre(){
+    const data = await this.apiservice.GetDataGenre()
+    for (let index = 0; index < data.length; index++) {
+      this.genres.push(data[index]);
+    }
+    
+  }
   async CreateCard() {
     this.cards = this.imgAlbums.map((img, index) => ({
       img: img,
-      title: this.nameAlbums[index]
+      title: this.genres[index]
     }));
   }
 }
